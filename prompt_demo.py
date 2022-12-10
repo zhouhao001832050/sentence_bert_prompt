93f0dc1ae79cf905975c43f116d2af0f1cdca4ca
hao.zhou <874212840@qq.com>
2022-12-10 19:27:19 +0800

    update dataprocessor

---
+++ prompt_demo.py
@@ -0,0 +1,17 @@
+from openprompt.data_utils import InputExample
+
+classes = [ # There are two classes in Sentiment Analysis, one for negative and one for positive
+    "negative",
+    "positive"
+]
+dataset = [ # For simplicity, there's only two examples
+    # text_a is the input text of the data, some other datasets may have multiple input sentences in one example.
+    InputExample(
+        guid = 0,
+        text_a = "Albert Einstein was one of the greatest intellects of his time.",
+    ),
+    InputExample(
+        guid = 1,
+        text_a = "The film was badly made.",
+    ),
+]
